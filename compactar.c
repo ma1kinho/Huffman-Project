6d9aba6f0f80d4b74760986bac9d59576014867c
will.will_will <52809519+ma1kinho@users.noreply.github.com>
2020-01-27 17:03:51 -0300

    Update compactar.c
    alguns testes22

--- compactar.c
+++ compactar.c
@@ -1,44 +1,97 @@
 #include "compactar.h"
 
+//---------------------------------TESTES-------------------------------------//
+
+void print_pre_order(NO *tree)
+{
+	if(tree != NULL){
+    	printf("%c", (char*)(tree->item) );
+        print_pre_order(tree->esq);
+    	print_pre_order(tree->dir);
+	}
+}
+
+void printar_fila(FILA *fila){
+    NO *auxiliar = fila->cabeca;
+
+    while(auxiliar->prox != NULL){
+        printf("%c-", (char*)(auxiliar->item));
+        auxiliar = auxiliar->prox;
+    }
+    printf("%c-", (char*)(auxiliar->item));
+}
+
+void printar_hash(HT *ht){
+    int i;
+
+    for(i=0; i<256; i++){
+        if((ht->tabela[i]->frequencia)>0){
+            printf("Caracter: %c\nCaminho: %s\nFrequencia: %d\n\n", i, ht->tabela[i]->caminho, ht->tabela[i]->frequencia);
+        }
+    }
+}
+
+//-------------------------------FIM TESTES----------------------------------//
+
+
+
 //Realiza todo o procedimento para compactar.
 void compactar()
 {
     char nome_do_arquivo[100], caminho[ALTURA_MAX];
-    int i, contador=0, *tamanho = 0;
-    unsigned char lixo, cabecalho;
+    int i, contador=0, inteiro;
+    unsigned short lixo, cabecalho, tamanho=0;
 
-    printf("Digite o nome do arquivo a ser compactado:\n");
+    printf("\nDigite o nome do arquivo a ser compactado:\n");
     scanf("%s", nome_do_arquivo);
-    fflush(stdin); //TALVEZ DÊ ERRO
 
     FILE *arquivo = fopen(nome_do_arquivo, "rb");
 
     if(arquivo == NULL){
-        printf("Erro no arquivo.\nERRO 017\n");
+        printf("\nErro no arquivo.\nERRO 017\n\n\n");
         return;
     }
-    printf("Iniciando processo de compactação...\n##############[0%]\n##############[13,7%]\n");
+    printf("Iniciando processo de compactação...\n##############[0%%]\n\n");
 
     HT *ht = criar_hash_table();
     adicionar_cada_frequencia(arquivo, ht);
     rewind(arquivo);
-    printf("\nProcesso em andamento...\n##############[21,9%]\n");
+    printf("\nProcesso em andamento...\n##############[21,9%%]\n\n");
     
     FILA *fila = criar_fila_vazia();
     fila = criar_fila_prioridade(ht, fila); //Fila aponta para a raiz da arvore.
+    printf("Printar Fila: ");
+    printar_fila(fila);
+    printf("\n\n");
+
     NO *arvore = criar_arvore_huffman(fila);
+    printf("Printar Arvore: ");
+    print_pre_order(arvore);
+    printf("\n\n");
+
     criar_caminho_na_hash(arvore, ht, caminho, contador);
-    printf("\nProcesso em andamento...\n##############[67,31%]\n");
+    printf("Printar Hash:\n\n");
+    printar_hash(ht);
+    printf("\nProcesso em andamento...\n##############[47,3%%]\n\n");
 
     lixo = calcula_tam_lixo(ht);
+    printf("Tamanho Lixo: %d\n", lixo);
+    inteiro = lixo;
     calcula_tam_arvore(arvore, &tamanho);
-    lixo = lixo << 13;
-    cabecalho = setar_bits(lixo, tamanho);
-    *nome_do_arquivo = strtok(nome_do_arquivo, ".");
-    *nome_do_arquivo = strcat(nome_do_arquivo, ".huff");
+    printf("Tamanho Arvore: %d\n", tamanho);
+    inteiro = inteiro << 13;
+    inteiro = inteiro | tamanho;
+    //printf("Cabecalho: %d\n", inteiro);
+    printf("\nProcesso em andamento...\n##############[75,2%%]\n\n");
+
+    *nome_do_arquivo = *strtok(nome_do_arquivo, ".");
+    *nome_do_arquivo = *strcat(nome_do_arquivo, ".huff");
     FILE *saida = fopen(nome_do_arquivo, "wb");
-    unsigned char aux = cabecalho;
-    fprintf(saida, "%c%c", cabecalho>>8, aux);
-    imprimir_pre_ordem(saida, arvore);
-    
+    printf("\nProcesso em andamento...\n##############[90%%]\n\n");
+
+    fputc(inteiro, saida);
+    fseek(saida, (2 + tamanho), SEEK_SET);
+    imprimir_pre_ordem(saida, arvore); //ERRO AQUI
+    imprimir_bits_dados(arquivo, saida, ht);
+    printf("\nProcesso concluido com sucesso!...\n\n\n\n");
 }
